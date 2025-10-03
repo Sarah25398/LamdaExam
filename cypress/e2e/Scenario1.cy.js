beeb3dd5bb21dab79c4648239f03ba07509125c2
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-10-03 10:32:17 +0800

    Update Scenario1.cy.js

--- cypress/e2e/Scenario1.cy.js
+++ cypress/e2e/Scenario1.cy.js
@@ -1,14 +1,11 @@
 describe('Scenario 1: Drag & Drop Sliders', () => {
   it('Test drag and drop to Slider value 15', () => {
     cy.visit('https://www.lambdatest.com/selenium-playground');
-
     cy.contains('Drag & Drop Sliders').click();
-
     cy.get("input[type='range'][value='15']")
       .invoke('val', 95)
       .trigger('input')
       .trigger('change');
-
     cy.get('#rangeSuccess').should('have.text', '95');
   });
 
